1049cb9474846fbf0b0807fff6fc6ddbf5800a9c
Milton Alvarado <1000tonaa@gmail.com>
2025-08-05 04:56:18 -0600

    Terminando algunas funciones de gramatica con transform

--- A-sintactico/Asintactico.py
+++ A-sintactico/Asintactico.py
@@ -1,11 +1,62 @@
 from lark import Lark, Transformer
 
+class T(Transformer):
+    # Métodos para transformar los nodos del árbol
+    # Cada método corresponde a una regla de la gramática
+    def NUMBER(self, valor):
+        return int(valor)
+    
+    def expr(self, valor):
+        return valor[0]
+    
+    def add(self, valor):
+        left, right = valor
+        return left + right
+    
+    def start(self, valor):
+        return valor[0] 
+  
+    def instruccion(self, valor):
+        return valor[0]
+    
+    def impresion(self, valor):
+        return f"Imprimir: {valor[0]}"
+    
+    def declarar(self, valor):
+        return f"Declarar: {valor[0]} = {valor[1]}"
+    
+    def asignacion(self, valor):
+        #Se lee de izq a der
+        variable = valor[0]
+        expresion = valor[1]
+        #Creo un diccionario para almacenar las variables y sus valores
+        self.variables[variable] = expresion
+        return f"Asignar: {variable} = {expresion}"
+    
+    def decision(self, valor):
+        #Se lee de arriba hacia abajo
+        ifstatement = valor[0]
+        condicion = valor[1]
+        bloque = valor[2]
+        elifparte = valor[3] if len(valor) > 3 else []
+        elseparte = valor[4] if len(valor) > 4 else None
+        return f"Decisión: {ifstatement} {condicion} {bloque}"
+        #Tengo que ver como se manejan los bloques de codigo con elif y else
+    
+    def ciclo(self, valor):
+        return f"Ciclo: {valor[0]} {valor[1]} {valor[2]}"
+
+
+
+
 with open('../Gramatica/grammar.Lark', 'r') as f:
     grammar = f.read()
     
 programa = """
   int edad = 1;
 """
+
+
 
 parser = Lark(grammar, parser='lalr', lexer='contextual', maybe_placeholders=False)
 
